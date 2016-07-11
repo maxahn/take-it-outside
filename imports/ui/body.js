cadf9eedeaf827043df73d12a029ba35744c6ace
maxahn <ahnmaximus@gmail.com>
2016-07-10 17:41:46 -0700

    changed arguments to rooms

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -3,7 +3,7 @@ import { Template } from 'meteor/templating';
 import './body.html';
 
 Template.body.helpers({
-  arguments: [
+  rooms: [
     { topic: 'Is chocolate the best icecream flavour?', creator: {name: 'Max', arguments: [{point: 'blah1', createdAt: new Date()},{point: 'blah2', createdAt: new Date()}]}, challenged: {name: 'Eamonn'}, createdAt: new Date()}, 
     { topic: 'Is Obama really American?', creator: {name: 'Saeid'}, challenged: {name: 'Deepak'}, createdAt: new Date()},
     { topic: 'Is Bugs Bunny really smart, or Elmer Fudd just really stupid?' , creator: {name: 'Bugs Bunny'}, challenged: {name: 'Elmer Fudd'}, createdAt: new Date()}
