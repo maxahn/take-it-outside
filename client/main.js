c3ded184fed459f42312a6b6f086c48375ba4f7b
maxahn <ahnmaximus@gmail.com>
2016-07-11 10:19:56 -0700

    moved material template to imports and routed landing page

--- client/main.js
+++ client/main.js
@@ -2,7 +2,11 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import '../imports/ui/body.js';
+//import '../imports/ui/body.js';
+import '../imports/material-design-template-master/www/index.html';
+
+Router.route('/', function() {
+  this.render('landingPage');
+});
 var $ = require("jquery"),
     materialize = require("materialize");
-
