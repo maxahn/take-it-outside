0e6da10fb3630a79082c5bb6b9109e104896ed8f
deemun <deepak.nayak93@gmail.com>
2016-07-12 11:53:11 -0700

    homepage ready, plugin working

---
+++ imports/ui/body.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating';
+
+import './body.html';
+// import '../custom-min.js';
+// import '../plugin-min.js';
+
+Template.homepage.onRendered(function(){
+
+
+
+});
