2f48e1e22e898a211cbb4155a2a36b1c9861a078
deemun <deepak.nayak93@gmail.com>
2016-07-13 16:34:46 -0700

    form done, route for form done

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,10 @@ import '../imports/ui/home_page/body.html';
 import '../imports/ui/main_debate/body.js';
 import '../imports/ui/main_debate/body.html';
 
+
+import '../imports/ui/form_page/body.js';
+import '../imports/ui/form_page/body.html'; //must require files to get recognized by router
+
 Router.route ('/', function() {
   this.render('homepage');
 });
@@ -12,3 +16,6 @@ Router.route('/test-room', function() {
   this.render('trumpRoom');
 });
 
+Router.route('/form', function() {
+  this.render('form');
+});
