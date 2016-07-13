55c5f6c937941612d5473e6066ee4f864c56666b
maxahn <ahnmaximus@gmail.com>
2016-07-13 11:24:20 -0700

    jquery plugins do not seem to be in effect but test room and homepage have been routed with iron router

--- client/main.js
+++ client/main.js
@@ -3,6 +3,5 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 import '../imports/ui/home_page/body.js';
-//import '../client/js/'    //what was this intended to do? 
 
 
