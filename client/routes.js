d659e9276c93fcc172e6408a102716b304c1798b
maxahn <ahnmaximus@gmail.com>
2016-07-13 12:50:42 -0700

    uncommented routes

--- client/routes.js
+++ client/routes.js
@@ -1,14 +1,14 @@
-// import '../imports/ui/home_page/body.js';
-// import '../imports/ui/home_page/body.html';
-//
-// import '../imports/ui/main_debate/body.js';
-// import '../imports/ui/main_debate/body.html';
-//
-// Router.route ('/', function() {
-//   this.render('homepage');
-// });
-//
-// Router.route('/test-room', function() {
-//   this.render('trumpRoom');
-// });
-//
+import '../imports/ui/home_page/body.js';
+import '../imports/ui/home_page/body.html';
+
+import '../imports/ui/main_debate/body.js';
+import '../imports/ui/main_debate/body.html';
+
+Router.route ('/', function() {
+  this.render('homepage');
+});
+
+Router.route('/test-room', function() {
+  this.render('trumpRoom');
+});
+
