b0bcde024af41848b4f0d07aa77eceaea386bf15
maxahn <ahnmaximus@gmail.com>
2016-07-13 14:19:14 -0700

    formatted debate topic and debaternames

--- imports/ui/main_debate/body.js
+++ imports/ui/main_debate/body.js
@@ -4,16 +4,16 @@ import { Rooms } from '../../api/rooms'
 import './body.css';
 import './body.html';
 
-Template.rooms.helpers({
-  rooms() {
-    return Rooms.find();
-  },
-
-  findRoom(url) {
-    return Rooms.find({url: url}); 
-  }
-});
-
+// Template.rooms.helpers({
+//   rooms() {
+//     return Rooms.find();
+//   },
+//
+//   findRoom(url) {
+//     return Rooms.find({url: url}); 
+//   }
+// });
+//
 Template.trumpRoom.helpers({
   room(url) {
     return Rooms.findOne({url: url});
