83a1b4d084ad89d812671df3f8ccf2ca67e51cc4
maxahn <ahnmaximus@gmail.com>
2016-07-20 14:54:10 -0700

    new facebook config

--- server/social-config.js
+++ server/social-config.js
@@ -4,6 +4,6 @@ ServiceConfiguration.configurations.remove({
  
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '1112733802130992',
-    secret: '421b026e5e1be3850185d56cd16627ac'
+    appId: '975440055907612',
+    secret: 'efb36c54f9e2663f75df27eb2780566e'
 });
